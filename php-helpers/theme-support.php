c995323726afa64cd15dc15c883d52d5d3b08362
Iain J McCallum <imccallum@delphicdigital.com>
2017-02-07 09:06:54 -0500

    cleanout

---
+++ php-helpers/theme-support.php
@@ -0,0 +1,62 @@
+<?php 
+// Adding WP 3+ Functions & Theme Support
+function bones_theme_support() {
+
+	// wp thumbnails (sizes handled in functions.php)
+	add_theme_support( 'post-thumbnails' );
+
+	// default thumb size
+	set_post_thumbnail_size(125, 125, true);
+
+	// wp custom background (thx to @bransonwerner for update)
+	add_theme_support( 'custom-background',
+	    array(
+	    'default-image' => '',    // background image default
+	    'default-color' => '',    // background color default (dont add the #)
+	    'wp-head-callback' => '_custom_background_cb',
+	    'admin-head-callback' => '',
+	    'admin-preview-callback' => ''
+	    )
+	);
+
+	// rss thingy
+	add_theme_support('automatic-feed-links');
+
+	// to add header image support go here: http://themble.com/support/adding-header-background-image-support/
+
+	// adding post format support
+	add_theme_support( 'post-formats',
+		array(
+			'aside',             // title less blurb
+			'gallery',           // gallery of images
+			'link',              // quick link to other site
+			'image',             // an image
+			'quote',             // a quick quote
+			'status',            // a Facebook like status update
+			'video',             // video
+			'audio',             // audio
+			'chat'               // chat transcript
+		)
+	);
+
+	// wp menus
+	add_theme_support( 'menus' );
+
+	// registering wp3+ menus
+	register_nav_menus(
+		array(
+			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
+			'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
+		)
+	);
+
+	// Enable support for HTML5 markup.
+	add_theme_support( 'html5', array(
+		'comment-list',
+		'search-form',
+		'comment-form'
+	) );
+
+} /* end bones theme support */
+
+?>
---
+++ php-helpers/thumbnail-image-sizes.php
@@ -0,0 +1,44 @@
+<?php
+
+// Thumbnail sizes
+add_image_size( 'bones-thumb-600', 600, 150, true );
+add_image_size( 'bones-thumb-300', 300, 100, true );
+
+/*
+to add more sizes, simply copy a line from above
+and change the dimensions & name. As long as you
+upload a "featured image" as large as the biggest
+set width or height, all the other sizes will be
+auto-cropped.
+
+To call a different size, simply change the text
+inside the thumbnail function.
+
+For example, to call the 300 x 100 sized image,
+we would use the function:
+<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+for the 600 x 150 image:
+<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
+
+You can change the names and dimensions to whatever
+you like. Enjoy!
+*/
+
+add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+
+function bones_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'bones-thumb-600' => __('600px by 150px'),
+        'bones-thumb-300' => __('300px by 100px'),
+    ) );
+}
+
+/*
+The function above adds the ability to use the dropdown menu to select
+the new images sizes you have just created from within the media manager
+when you add media to your content blocks. If you add more image sizes,
+duplicate one of the lines in the array and name it according to your
+new image size.
+*/
+
+?>
