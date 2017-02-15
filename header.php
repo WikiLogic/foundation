879624217bb8c9d4ba26f33df730682c5ca5820a
ijmccallum <ijmccallum@hotmail.co.uk>
2017-02-14 21:04:56 -0500

    header in disarray, ugh - so damn difficult sometimes!

--- header.php
+++ header.php
@@ -36,7 +36,8 @@
 		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 			<div class="header__top">
-					
+				
+				<!-- static section -->
 				<label class="header__menu-button" for="menu-expander">
 					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 54 54">
 						<circle cx="7" cy="47" r="7"/>
@@ -54,8 +55,9 @@
 				<div class="header__title">
 					<a href="<?php echo home_url(); ?>" class="header__title-text" itemscope itemtype="http://schema.org/Organization"><?php bloginfo('name'); ?></a>
 				</div>
-					
-				<div class="header__search-button">
+				
+				<label class="header__search-close" for="search-toggler">
+					<!-- Maginfying glass -->
 					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 250.313 250.313" >
 						<path style="fill-rule:evenodd;clip-rule:evenodd;" d="M244.186,214.604l-54.379-54.378c-0.289-0.289-0.628-0.491-0.93-0.76
 							c10.7-16.231,16.945-35.66,16.945-56.554C205.822,46.075,159.747,0,102.911,0S0,46.075,0,102.911
@@ -64,19 +66,41 @@
 							c-37.134,0-67.236-30.102-67.236-67.235c0-37.134,30.103-67.236,67.236-67.236c37.132,0,67.235,30.103,67.235,67.236
 							C170.146,140.044,140.043,170.146,102.911,170.146z"/>
 					</svg>
-				</div>
-
-				<!-- <?php get_search_form(true); ?> -->
+				</label>
+				<!-- END static section -->
 
 			</div>
-
-			<input type="checkbox" id="menu-expander" class="header__checkbox-expander"/>
-			<div class="header__nav-area">
 			
-				<nav class="main-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-					<?php require('php-helpers/navigation.php'); ?>
-				</nav>
+			<input type="checkbox" id="search-toggler" class="header__search-toggler"/>
+			<div class="header__top-search">
+				<!-- static section -->
+				<label class="header__menu-button" for="menu-expander">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 54 54">
+						<circle cx="7" cy="47" r="7"/>
+						<circle cx="27" cy="47" r="7"/>
+						<circle cx="47" cy="47" r="7"/>
+						<circle cx="7" cy="27" r="7"/>
+						<circle cx="27" cy="27" r="7"/>
+						<circle cx="47" cy="27" r="7"/>
+						<circle cx="7" cy="7" r="7"/>
+						<circle cx="27" cy="7" r="7"/>
+						<circle cx="47" cy="7" r="7"/>
+					</svg>
+				</label>
 
+				<?php get_search_form(true); ?>
+
+				<label class="header__search-close" for="search-toggler">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink">
+						<path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>
+					</svg>
+				</label>
 			</div>
-
 		</header>
+		
+		<input type="checkbox" id="menu-toggler" class="header__bottom-toggler"/>
+		<div class="header__bottom">
+			<nav class="main-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+				<?php require('php-helpers/navigation.php'); ?>
+			</nav>
+		</div>
--- searchform.php
+++ searchform.php
@@ -1,8 +1,10 @@
+<?php require('php-helpers/svg-icons.php'); ?>
+
 <form role="search" method="get" id="searchform" class="search" action="<?php echo home_url( '/' ); ?>">
     <label class="search__label">
-        <span class="search__label-text"><?php _e('Search for:','bonestheme'); ?></span>
+        <!-- <span class="search__label-text"><?php _e('Search for:','bonestheme'); ?></span> -->
         <input type="search" class="search__input" id="s" name="s" value="" placeholder="<?php _e('Search','bonestheme'); ?>" />
     </label>
 
-    <button type="submit" id="searchsubmit" class="search__submit"><?php _e('Search','bonestheme'); ?></button>
+    <button type="submit" id="searchsubmit" class="search__submit"><?php print $SvgIcons->search(); ?></button>
 </form>
